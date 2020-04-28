a66fd90b328b35867ed28f080e83e5330f19a960
GUMEIK <13103615283@163.com>
2020-04-28 13:35:59 +0800

    首页增加骨架屏

--- src/index.js
+++ src/index.js
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Skeleton } from 'antd';
+document.onreadystatechange = ()=>{
+  if(document.readyState === "complete"){
+    // 资源加载完成
+    ReactDOM.render(
+      // <React.StrictMode>
+      //   <App />
+      // </React.StrictMode>,
+      <App />,
+    
+      document.getElementById('root')
+    );
+  }else{//资源加载中
+    ReactDOM.render(
+      <>
+        <Skeleton active={true}/>,
+        <Skeleton />,
+        <Skeleton />,
+        <Skeleton />,
+      </>,
+      document.getElementById('root')
+    );
+  }
+}
 
-ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <App />,
-
-  document.getElementById('root')
-);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
